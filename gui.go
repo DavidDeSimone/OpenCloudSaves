4f2f6989860d23b16c58c6c4b5ce60ccb6df95f4
David DeSimone <Lord.Good.Mail@gmail.com>
2022-12-31 15:12:44 -0800

    Removing unused function

--- gui.go
+++ gui.go
@@ -5,70 +5,17 @@ import (
 	"fmt"
 	"image/color"
 	"os"
-	"time"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/container"
-	"fyne.io/fyne/driver/desktop"
-	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
 
 //go:embed icon.jpg
 var icon []byte
-
-func windowScreen(_ fyne.Window) fyne.CanvasObject {
-	windowGroup := container.NewVBox(
-		widget.NewButton("New window", func() {
-			w := fyne.CurrentApp().NewWindow("Hello")
-			w.SetContent(widget.NewLabel("Hello World!"))
-			w.Show()
-		}),
-		widget.NewButton("Fixed size window", func() {
-			w := fyne.CurrentApp().NewWindow("Fixed")
-			w.SetContent(fyne.NewContainerWithLayout(layout.NewCenterLayout(), widget.NewLabel("Hello World!")))
-
-			w.Resize(fyne.NewSize(240, 180))
-			w.SetFixedSize(true)
-			w.Show()
-		}),
-		widget.NewButton("Centered window", func() {
-			w := fyne.CurrentApp().NewWindow("Central")
-			w.SetContent(fyne.NewContainerWithLayout(layout.NewCenterLayout(), widget.NewLabel("Hello World!")))
-
-			w.CenterOnScreen()
-			w.Show()
-		}))
-
-	drv := fyne.CurrentApp().Driver()
-	if drv, ok := drv.(desktop.Driver); ok {
-		windowGroup.Objects = append(windowGroup.Objects,
-			widget.NewButton("Splash Window (only use on start)", func() {
-				w := drv.CreateSplashWindow()
-				w.SetContent(widget.NewLabelWithStyle("Hello World!\n\nMake a splash!",
-					fyne.TextAlignCenter, fyne.TextStyle{Bold: true}))
-				w.Show()
-
-				go func() {
-					time.Sleep(time.Second * 3)
-					w.Close()
-				}()
-			}))
-	}
-
-	otherGroup := widget.NewCard("Other", "",
-		widget.NewButton("Notification", func() {
-			fyne.CurrentApp().SendNotification(&fyne.Notification{
-				Title:   "Fyne Demo",
-				Content: "Testing notifications...",
-			})
-		}))
-
-	return container.NewVBox(widget.NewCard("Windows", "", windowGroup), otherGroup)
-}
 
 func makeButtonList(count int) []fyne.CanvasObject {
 	var items []fyne.CanvasObject
@@ -241,9 +188,6 @@ func GuiMain(ops *Options, dm *GameDefManager) {
 	horiz := container.NewHScroll(container.NewHBox(hlist...))
 	vert := container.NewVScroll(container.NewVBox(vlist...))
 
-	// cont := container.NewAdaptiveGrid(2,
-	// 	container.NewBorder(horiz, nil, nil, nil, vert),
-	// 	plainContainer)
 	hsplit := container.NewHSplit(vert, plainContainer)
 	hsplit.Offset = 0.2
 
@@ -251,6 +195,5 @@ func GuiMain(ops *Options, dm *GameDefManager) {
 	cont.Offset = 0.1
 
 	w.SetContent(cont)
-	// w.SetContent(makeSplitTab(w))
 	w.ShowAndRun()
 }
