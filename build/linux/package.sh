f75d3b06e5545e88120f07a4b2821ce4237a50f8
David DeSimone <Lord.Good.Mail@gmail.com>
2023-01-20 01:10:26 -0800

    More iteration on building to repo

--- build/linux/package.sh
+++ build/linux/package.sh
@@ -1,4 +1,4 @@
 flatpak uninstall --user org.github.opencloudsaves.opencloudsaves
-flatpak-builder --force-clean --disable-rofiles-fuse --user --install-deps-from --repo=repo build-dir org.github.opencloudsaves.opencloudsaves.yml
-flatpak --user remote-add --if-not-exists --no-gpg-verify tutorial-repo repo
-flatpak --user install tutorial-repo org.github.opencloudsaves.opencloudsaves
+flatpak-builder --force-clean --user --disable-rofiles-fuse --repo=repo build-dir org.github.opencloudsaves.opencloudsaves.yml
+#flatpak --user remote-add --if-not-exists --no-gpg-verify tutorial-repo repo
+flatpak  install --user tutorial-repo org.github.opencloudsaves.opencloudsaves
