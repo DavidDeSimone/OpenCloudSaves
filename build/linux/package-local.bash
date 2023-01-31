40df16fec78a831b6e3e359683ad3c0f13cd5da6
David DeSimone <lordg@david>
2023-01-31 00:54:09 -0800

    Fixing up local build for flatpak - working on steam deck!

--- build/linux/package-local.bash
+++ build/linux/package-local.bash
@@ -2,4 +2,6 @@
 
 set -e
 
-flatpak-builder --force-clean --install --user ./build/linux/build-dir ./build/linux/org.github.opencloudsaves.opencloudsaves.local.yml
+# Requied due to https://vielmetti.typepad.com/logbook/2022/10/git-security-fixes-lead-to-fatal-transport-file-not-allowed-error-in-ci-systems-cve-2022-39253.html
+git config protocol.file.allow always
+flatpak-builder --force-clean --user --install --repo=repo ./build/linux/build-dir ./build/linux/org.github.opencloudsaves.opencloudsaves.local.yml
