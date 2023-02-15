19d3a1af8654ce76150321e8fdb39ea60318ec75
David DeSimone <Lord.Good.Mail@gmail.com>
2023-02-14 21:20:37 -0800

    Adding install script

--- build/linux/install.bash
+++ build/linux/install.bash
@@ -1,23 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if ! (zenity --question --title="Disclaimer" --text="This software is in a open beta. If you have valuable save data, it is recommended that you make a manual backup. OpenCloudSave is not responsible for lost data. Do you agree and wish to continue?" --width=600 2> /dev/null); then
-    zenity --error --title="Terms Denied" --text="Terms were denied, cannot proceed." --width=300 2> /dev/null
+set -e
+
+if ! (zenity --question --title="Welcome" --text="This software is in a open beta. If you have valuable save data, it is recommended that you make a manual backup. OpenCloudSave is not responsible for lost data. Do you agree and wish to continue?" --width=500 2> /dev/null); then
+    zenity --error --title="Usaged Denied" --text="Not proceeding further with install. Run this script again if you change your mind. Thank you." --width=500 2> /dev/null
     exit 1
 fi
-hasPass=$(passwd -S "$USER" | awk -F " " '{print $2}')
-if [[ $hasPass != "P" ]]; then
+
+passIsSet=$(passwd -S "$USER" | awk -F " " '{print $2}')
+if [[ $passIsSet != "P" ]]; then
     zenity --error --title="Password Error" --text="Password is not set, please set one in the terminal with the <b>passwd</b> command, then run this again." --width=400 2> /dev/null
     exit 1
 fi
-PASSWD="$(zenity --password --title="Enter Password" --text="Enter Deck User Password (not Steam account!)" 2>/dev/null)"
-echo "$PASSWD" | sudo -v -S
-ans=$?
-if [[ $ans == 1 ]]; then
-    zenity --error --title="Password Error" --text="Incorrect password provided, please run this command again and provide the correct password." --width=400 2> /dev/null
-    exit 1
-fi
+
 mkdir -p /tmp/
-curl https://github.com/DavidDeSimone/OpenCloudSaves/releases/download/v0.16.0/linux_opencloudsaves_0.16.0_x86_64.flatpak > /tmp/opencloudsaves.flatpak
-flatpak install --noninteractive --or-update /tmp/opencloudsaves.flatpak
-curl https://github.com/DavidDeSimone/OpenCloudSaves/releases/download/v0.16.0/OpenCloudSave.desktop > ~/Desktop/OpenCloudSave.desktop
-#curl desktop file, put on desktop
+curl -L "https://github.com/DavidDeSimone/OpenCloudSaves/releases/download/v0.16.0/linux_opencloudsaves_0.16.0_x86_64.flatpak" > /tmp/opencloudsaves.flatpak
+flatpak install --user --noninteractive --or-update /tmp/opencloudsaves.flatpak
+
+if [[ -d ~/Desktop/ ]]; then
+    curl -L "https://github.com/DavidDeSimone/OpenCloudSaves/releases/download/v0.16.0/OpenCloudSave.desktop" > ~/Desktop/OpenCloudSave.desktop
+else 
+    echo 'Skipping desktop file installation - Desktop not found.'
+fi
