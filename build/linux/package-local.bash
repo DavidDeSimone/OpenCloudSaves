ddb43e4b017e8ac6c6d58fa9a67126ccc9a0c748
David DeSimone <Lord.Good.Mail@gmail.com>
2023-02-20 22:06:23 -0800

    Updating flatpak yml with appdata files

--- build/linux/package-local.bash
+++ build/linux/package-local.bash
@@ -5,4 +5,4 @@ set -e
 # Requied due to https://vielmetti.typepad.com/logbook/2022/10/git-security-fixes-lead-to-fatal-transport-file-not-allowed-error-in-ci-systems-cve-2022-39253.html
 git config protocol.file.allow always
 flatpak-builder --force-clean --user --install --repo=repo ./build/linux/build-dir ./build/linux/io.github.daviddesimone.opencloudsaves.local.yml
-# flatpak build-bundle repo opencloudsave.flatpak io.github.daviddesimone.opencloudsaves
+flatpak build-bundle repo opencloudsave.flatpak io.github.daviddesimone.opencloudsaves
