443fe9b11f91dcb6af3b69cc598e2de1c92d942b
David DeSimone <Lord.Good.Mail@gmail.com>
2023-03-10 20:21:30 -0800

    Further updates to the MSI builder for a better user experience.

--- build/win/package.sh
+++ build/win/package.sh
@@ -6,6 +6,6 @@ CURR=$(pwd)
 cd build/win/
 
 "/c/Program Files (x86)/WiX Toolset v3.11/bin/candle.exe" opencloudsave.wxs
-"/c/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" -ext WixUIExtension -cultures:en-us opencloudsave.wixobj
+"/c/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" -ext WixUIExtension -ext WixUtilExtension -cultures:en-us opencloudsave.wixobj
 
 cd $CURR
