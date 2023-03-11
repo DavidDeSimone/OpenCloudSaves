6e70db7f47b564863e545e379a8566bd12a5091a
David DeSimone <Lord.Good.Mail@gmail.com>
2023-03-10 19:23:32 -0800

    Initial commit for improving msi installer.

--- build/win/package.sh
+++ build/win/package.sh
@@ -6,6 +6,6 @@ CURR=$(pwd)
 cd build/win/
 
 "/c/Program Files (x86)/WiX Toolset v3.11/bin/candle.exe" opencloudsave.wxs
-"/c/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" opencloudsave.wixobj
+"/c/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" -ext WixUIExtension -cultures:en-us opencloudsave.wixobj
 
 cd $CURR
